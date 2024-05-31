f68f89960fa978dd5168b15b096da7b7baa4ae55
Zeeta:) <126560008+zzz3t444@users.noreply.github.com>
2024-05-31 00:52:14 -0700

    New UI section

--- application/views/home.php
+++ application/views/home.php
@@ -41,7 +41,8 @@
         <div class="flex items-center mt-32 bg-neutral-950 border-b border-t border-neutral-900 p-20 justify-center gap-20">
             <div>
                 <h1 class="text-5xl font-semibold dm-sans text-start text-neutral-200 leading-tight">Thanks for <br>
-                <span class="text-3xl">SMK MUHAMMADYAH 1 YOGYAKARTA</span></h1>
+                    <span class="text-3xl">SMK MUHAMMADYAH 1 YOGYAKARTA</span>
+                </h1>
                 <p class="max-w-xl text-neutral-200 text-lg mt-6 archivo text-start leading-10">I am a student of SMK MUHAMMADYAH 1 YOGYAKARTA who majors in Software Engineering. I really like majoring in Software Engineering because the digital world is very broad.</p>
             </div>
             <div><img src="https://res.cloudinary.com/ddy7p8yrj/image/upload/v1717001528/pf2frcn8tujynmvxqrho.png" width="550px" alt=""></div>
@@ -71,14 +72,16 @@
     <section title="support-people-programmer">
         <div class="flex items-center justify-center mt-10 gap-20">
             <div class="flex items-center">
-                <img src="https://avatars.githubusercontent.com/u/113794999?v=4" class="border rounded-full relative border-neutral-800" width="400px" alt="">
-                <img src="https://avatars.githubusercontent.com/u/100127440?v=4" class="border rounded-full -ml-40 mt-60 border-neutral-800" width="400px" alt="">
+                <div class="flex items-center relative">
+                    <img src="https://res.cloudinary.com/ddy7p8yrj/image/upload/v1717141387/cqqazwvbpvfxrrnkjbrd.png" class="border bg-black  rounded-full relative border-neutral-800" width="400px" alt="">
+                    <img src=" https://avatars.githubusercontent.com/u/113794999?v=4" class="border rounded-full -ml-40 mt-56 border-neutral-800" width="400px" alt="">
+                </div>
+                <img src="https://avatars.githubusercontent.com/u/100127440?v=4" class="border -z-10 rounded-full -ml-40 mt-[500px] border-neutral-800" width="400px" alt="">
             </div>
             <div>
                 <h1 class="text-neutral-200 dm-sans font-semibold text-5xl mt-28 text-start leading-tight">participants Contributions to <br>
                     this website</h1>
-                <p class="max-w-2xl text-neutral-200 text-lg mt-6 archivo text-start leading-10">This website was created with great collaboration in terms of website layout design to database needs.
-                    we invited <span class="text-sky-600">@z3t444</span> to provide website design inspiration. and we invited <span class="text-sky-600">@dzakyabdurhmn</span> to determine a data relationship for this website. we are very grateful to these contributors.</p>
+                <p class="max-w-2xl text-neutral-200 text-lg mt-6 archivo text-start leading-10">This website was created with good collaboration in terms of website layout design to database needs. we invited <span class="text-sky-600">@z3t444</span> to provide inspiration for website design, we invited <span class="text-sky-600">@dzakyabdurhmn</span> to determine data relations on this website, and we invited <span class="text-sky-600">@ridhoarrahmann</span> for consultation on the problems of making this website. we are very grateful to these contributors.</p>
             </div>
         </div>
     </section>
