5e4df6e30b4004e9fba3f138c0b251f7bf61002a
ridhoarrahman <ridhoarrahman7@gmail.com>
2024-06-03 14:13:11 +0700

    update

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,20 +9,21 @@ class Auth extends CI_Controller
 
 	public function login()
 	{
+		// login
 		$this->load->model('auth_model');
 		$this->load->library('form_validation');
 
 		$rules = $this->auth_model->rules();
 		$this->form_validation->set_rules($rules);
 
-		if($this->form_validation->run() == FALSE){
+		if ($this->form_validation->run() == FALSE) {
 			return $this->load->view('login_form');
 		}
 
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 
-		if($this->auth_model->login($username, $password)){
+		if ($this->auth_model->login($username, $password)) {
 			redirect('admin');
 		} else {
 			$this->session->set_flashdata('message_login_error', 'Login Gagal, pastikan username dan password benar!');
